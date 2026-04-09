a31d136b57d2bf74a162127e70578278b5244ec6
brendan <brendanrkent@yahoo.ie>
2026-04-09 23:39:14 +0100

    remove not needed stuff

--- app.py
+++ app.py
@@ -2,35 +2,6 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-videos = [
-    {
-        "url": "https://res.cloudinary.com/dkheyxxzk/video/upload/v1775246414/vauq4o_xhtmy9.mp4",
-        "caption": "Badger best"
-    },
-    {
-        "url": "https://res.cloudinary.com/dkheyxxzk/video/upload/v1775246413/izjoty_tkg9xn.mp4",
-        "caption": "Badger also"
-    },
-    {
-        "url": "https://res.cloudinary.com/dkheyxxzk/video/upload/v1775246410/j6wxck_g9ps3t.mp4",
-        "caption": "Another Badger"
-    },
-    {
-        "url": "https://res.cloudinary.com/dkheyxxzk/video/upload/v1775246409/9uenlv_ntdjg5.mp4",
-        "caption": "Again a Badger"
-    }
-]
-
-
-@app.route('/')
-def home():
-    return render_template('index.html', media=media_items)
-
-
-from flask import Flask, render_template
-
-app = Flask(__name__)
-
 media_items = [
     {
         "type": "video",
