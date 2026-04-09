f2882db6d3d92244f5ad0fe757ae205f372981c6
brendan <brendanrkent@yahoo.ie>
2026-04-09 20:45:25 +0100

    add little caption, forget one thing

--- app.py
+++ app.py
@@ -2,13 +2,6 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# Add your Cloudinary links here
-video_urls = [
-    "https://res.cloudinary.com/dkheyxxzk/video/upload/v1775246414/vauq4o_xhtmy9.mp4",
-    "",
-    "",
-    "https://res.cloudinary.com/dkheyxxzk/video/upload/v1775246409/9uenlv_ntdjg5.mp4"
-]
 videos = [
     {
         "url": "https://res.cloudinary.com/dkheyxxzk/video/upload/v1775246414/vauq4o_xhtmy9.mp4",
@@ -29,4 +22,4 @@ videos = [
 ]
 @app.route('/')
 def home():
-    return render_template('index.html', video_urls=video_urls)
+    return render_template('index.html', videos=videos)
