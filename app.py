a01915c6d75ab2964ac559e9a797ffaf386be955
brendan <brendanrkent@yahoo.ie>
2026-04-09 20:43:22 +0100

    add little caption

--- app.py
+++ app.py
@@ -5,11 +5,28 @@ app = Flask(__name__)
 # Add your Cloudinary links here
 video_urls = [
     "https://res.cloudinary.com/dkheyxxzk/video/upload/v1775246414/vauq4o_xhtmy9.mp4",
-    "https://res.cloudinary.com/dkheyxxzk/video/upload/v1775246413/izjoty_tkg9xn.mp4",
-    "https://res.cloudinary.com/dkheyxxzk/video/upload/v1775246410/j6wxck_g9ps3t.mp4",
+    "",
+    "",
     "https://res.cloudinary.com/dkheyxxzk/video/upload/v1775246409/9uenlv_ntdjg5.mp4"
 ]
-
+videos = [
+    {
+        "url": "https://res.cloudinary.com/dkheyxxzk/video/upload/v1775246414/vauq4o_xhtmy9.mp4",
+        "caption": "Badger best"
+    },
+    {
+        "url": "https://res.cloudinary.com/dkheyxxzk/video/upload/v1775246413/izjoty_tkg9xn.mp4",
+        "caption": "Badger also"
+    },
+    {
+        "url": "https://res.cloudinary.com/dkheyxxzk/video/upload/v1775246410/j6wxck_g9ps3t.mp4",
+        "caption": "Another Badger"
+    },
+    {
+        "url": "https://res.cloudinary.com/dkheyxxzk/video/upload/v1775246409/9uenlv_ntdjg5.mp4",
+        "caption": "Again a Badger"
+    }
+]
 @app.route('/')
 def home():
     return render_template('index.html', video_urls=video_urls)
